a2c9553226e1a9bc5cbf840a0442264e4e2d8e77
Dori <58356939+Betterslash@users.noreply.github.com>
2020-12-05 17:25:18 +0200

    Add files via upload

--- console_3.sql
+++ console_3.sql
@@ -1,125 +1,125 @@
-/*a. modify the type of a column;*/
-CREATE OR ALTER PROCEDURE up_version_1
-AS
-    ALTER TABLE goodies_table
-    ALTER COLUMN nameOfGoodie varchar(255);
-    PRINT ('Set to version 1!');
-GO;
-EXEC up_version_1;
-CREATE OR ALTER PROCEDURE down_version_0
-AS
-    SET NOCOUNT ON;
-    ALTER TABLE goodies_table
-    ALTER COLUMN nameOfGoodie varchar(50);
-    PRINT('Back to version 0')
-GO;
-EXEC down_version_0;
+DELETE FROM TestViews;
+DELETE FROM TestTables;
+DELETE FROM Views;
+DELETE FROM TestRunTables;
+DELETE FROM TestRunViews;
+DELETE FROM TestRuns;
+DELETE FROM Tests;
+DELETE FROM Tables;
 
-CREATE OR ALTER PROCEDURE display
-AS
-    SET NOCOUNT ON;
-    SELECT * FROM goodies_table;
-GO;
-EXEC display;
+DBCC CHECKIDENT(Tables,RESEED, 0);
+DBCC CHECKIDENT(Tests,RESEED, 0);
+DBCC CHECKIDENT(Views,RESEED, 0);
 
-/*b. add / remove a column;*/
-CREATE OR ALTER PROCEDURE down_version_1
-AS
-    ALTER TABLE country_table
-    DROP COLUMN populationNumber;
-    PRINT ('Back to version 1!');
-GO;
-EXEC down_version_1;
+INSERT INTO Tables(Name) values('producer_table');
+INSERT INTO Tables(Name) values('games_table');
+INSERT INTO Tables(Name) values('producer_game_table');
+SELECT * FROM Tables;
 
-CREATE OR ALTER PROCEDURE up_version_2
-AS
-    ALTER TABLE country_table
-    ADD populationNumber int;
-    PRINT ('Set to version 2!');
-GO;
-EXEC up_version_2;
+INSERT INTO Views(Name) VALUES ('View_1');
+INSERT INTO Views(Name) VALUES ('View_2');
+INSERT INTO Views(Name) VALUES ('View_3');
+SELECT * FROM Views;
 
-/*c. add / remove a DEFAULT constraint;*/
-CREATE OR ALTER PROCEDURE down_version_2
-AS
-    alter table staff_table drop constraint salary;
-    PRINT ('Back to version 2!');
-GO;
-EXEC down_version_2;
+INSERT INTO Tests(Name) VALUES ('delete_table');
+INSERT INTO Tests(Name) VALUES ('insert_table');
+INSERT INTO Tests(Name) VALUES ('select_view');
+SELECT * FROM Tests;
 
-CREATE OR ALTER PROCEDURE up_version_3
-AS
-      ALTER TABLE staff_table
-      ADD CONSTRAINT salary DEFAULT 1000 FOR salary;
-      PRINT ('Set to version 3!');
-GO;
-EXEC up_version_3;
-
-/*d. add / remove a primary key;*/
-CREATE OR ALTER PROCEDURE up_version_4
-AS
-    ALTER TABLE sales_table
-    DROP CONSTRAINT PK_Sale;
-    PRINT ('Set to version 4!');
-GO;
-EXEC up_version_4;
-
-CREATE OR ALTER PROCEDURE down_version_3
-AS
-    ALTER TABLE sales_table
-    ADD CONSTRAINT PK_Sale PRIMARY KEY (saleID,productCode);
-    PRINT ('Back to version 3!');
-GO;
-EXEC down_version_3;
-/*e. add/remove candidate key*/
-CREATE OR ALTER PROCEDURE down_version_4
-AS
-    ALTER TABLE producer_table
-    DROP COLUMN phone_number;
-    PRINT ('Back to version 4!');
-GO;
-EXEC down_version_4;
-
-CREATE OR ALTER PROCEDURE up_version_5
-AS
-    ALTER TABLE producer_table
-    ADD phone_number int;
-    PRINT ('Set to version 5!');
-GO;
-EXEC up_version_5;
-
-/*f. add / remove a foreign key;*/
-CREATE OR ALTER PROCEDURE down_version_5
-AS
-    SET NOCOUNT ON;
-    alter table sales_table
-    drop constraint fk_customer_CNP;
-    PRINT ('Back to version 5');
-GO;
-EXEC down_version_5;
-CREATE OR ALTER PROCEDURE up_version_6
-AS
-    alter table sales_table
-    add constraint fk_customer_CNP FOREIGN KEY (customerCNP) references customers_table(CNP);
-    PRINT ('Set to version 5!');
-GO;
-EXEC up_version_6;
-
-/*g. create / drop a table.*/
-CREATE OR ALTER PROCEDURE up_version_7
-AS
-    create table continents_table(
-        cont_id int primary key ,
-        cont_countries int NOT NULL
-    );
-    PRINT ('Set to version 7!');
-GO;
-EXEC up_version_7;
-CREATE OR ALTER PROCEDURE down_version_6
-AS
-    drop table continents_table;
-    PRINT ('Back to version 6!');
-GO;
-EXEC down_version_6;
+INSERT INTO TestViews(TestID, ViewID) VALUES (3, 1);
+INSERT INTO TestViews(TestID, ViewID) VALUES (3, 2);
+INSERT INTO TestViews(TestID, ViewID) VALUES (3, 3);
+SELECT * FROM TestViews;
 
 
+DECLARE @all_start DATETIME;
+SET @all_start = GETDATE();
+
+DECLARE @delete_pg_start DATETIME;
+SET @delete_pg_start = GETDATE();
+EXEC delete_elems 'producer_game_table', 100;
+DECLARE @delete_pg_end DATETIME;
+SET @delete_pg_end = GETDATE();
+
+DECLARE @delete_g_start DATETIME;
+SET @delete_g_start = GETDATE();
+EXEC delete_elems 'games_table', 100;
+DECLARE @delete_g_end DATETIME;
+SET @delete_g_end = GETDATE();
+
+
+DECLARE @delete_p_start DATETIME;
+SET @delete_p_start = GETDATE();
+EXEC delete_elems 'producer_table', 100;
+DECLARE @delete_p_end DATETIME;
+SET @delete_p_end = GETDATE();
+
+
+DECLARE @insert_p_start DATETIME;
+SET @insert_p_start = GETDATE();
+EXEC insert_100_elems_producer;
+DECLARE @insert_p_end DATETIME;
+SET @insert_p_end = GETDATE();
+
+DECLARE @insert_g_start DATETIME;
+SET @insert_g_start = GETDATE();
+EXEC insert_100_elems_games;
+DECLARE @insert_g_end DATETIME;
+SET @insert_g_end = GETDATE();
+
+DECLARE @insert_pg_start DATETIME;
+SET @insert_pg_start = GETDATE();
+EXEC insert_100_elems_pg_table;
+DECLARE @insert_pg_end DATETIME;
+SET @insert_pg_end = GETDATE();
+
+DECLARE @view_1_start DATETIME;
+SET @view_1_start = GETDATE();
+EXEC select_view 1;
+DECLARE @view_1_end DATETIME;
+SET @view_1_end = GETDATE();
+
+DECLARE @view_2_start DATETIME;
+SET @view_2_start = GETDATE();
+EXEC select_view 2;
+DECLARE @view_2_end DATETIME;
+SET @view_2_end = GETDATE();
+
+DECLARE @view_3_start DATETIME;
+SET @view_3_start = GETDATE();
+EXEC select_view 3;
+DECLARE @view_3_end DATETIME;
+SET @view_3_end = GETDATE();
+
+DECLARE @all_stop DATETIME
+SET @all_stop = GETDATE();
+
+declare @description varchar(100)
+set @description = 'TestRun' + convert(varchar(7), (select max(TestRunID) from TestRuns)) + 'delete, insert 100 rows, select all views'
+
+insert into TestRuns(Description, StartAt, EndAt)
+values(@description, @all_start, @all_stop);
+
+declare @lastTestRunID int;
+set @lastTestRunID = (select max(TestRunID) from TestRuns);
+
+insert into TestRunTables
+values(@lastTestRunID, 1, @delete_pg_start, @insert_pg_end);
+
+insert into TestRunTables
+values(@lastTestRunID, 2, @delete_g_start, @insert_g_end);
+
+insert into TestRunTables
+values(@lastTestRunID, 3, @delete_p_start, @insert_p_end);
+
+insert into TestRunViews
+values(@lastTestRunID, 1, @view_1_start, @view_1_end);
+
+insert into TestRunViews
+values(@lastTestRunID, 2, @view_2_start, @view_2_end);
+
+insert into TestRunViews
+values(@lastTestRunID,3, @view_3_start, @view_3_end);
+
+SELECT *
+FROM TestRunTables;
