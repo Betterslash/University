f4a303b009cd6b69b50a67ded2619e8a1ae24ed3
Dori <58356939+Betterslash@users.noreply.github.com>
2021-01-09 16:16:30 +0200

    seems fine again

--- PLF/preg_exam.lisp
+++ PLF/preg_exam.lisp
@@ -2,7 +2,7 @@
     (cond
         ((null L) nil)
         ;((FUNCALL F (car L)) (CONS (FUNCALL F (CAR L)) (Fct F (CDR L))))
-        ((MAPCAR F L))
+        ((APPLY F L))
         (T nil)
     )
 )
@@ -11,3 +11,25 @@
 (setq Q 'G)
 (setq P Q)
 (print (FUNCALL P '(A B C)))
+
+
+;search_r(e, (a1, ..., an)) = { (), if |a1, ..., an| = 0
+;                            { true, if a1 = e
+;                            { search_r(e, (a1, ..., an)), otherwise
+(defun search_r (e L)
+    (cond 
+        ((null L) nil)
+        ((equal e (car L)) T)
+        ((search_r e (cdr L)))
+    )
+)
+
+;liniarize((a1, ..., an)) = { (a1), if a1 is Atom
+;                         = { U[i->n](a1, ..., an), if ai E (a1,...,an) 
+(defun liniarize (L)
+    (cond
+        ((atom L) (list l))
+        (T (MAPCAN #'liniarize L))
+    )
+)
+(print (search_r 'Z (liniarize '(a (b (c)) (d) (E (f))))))
