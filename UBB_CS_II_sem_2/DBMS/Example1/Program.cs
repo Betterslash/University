2c995d8eb5881c63e857df058815afad5dc510d3
Dori <58356939+Betterslash@users.noreply.github.com>
2021-02-23 14:03:21 +0200

    added DBMS

---
+++ UBB_CS_II_sem_2/DBMS/Example1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Example1
+{
+    class Program
+	{
+		static void Main(string[] args) 
+		{
+			SqlConnection sqlConnection = new SqlConnection();
+			SqlCommand sqlCommand = new SqlCommand();
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
